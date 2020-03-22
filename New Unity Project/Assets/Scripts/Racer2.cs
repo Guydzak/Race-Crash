2bd3dfb306e162cbfc63b6c09f841e3d96982c42
Christopher Mardiryan <chrismardiryan@yahoo.com>
2020-03-22 14:14:06 +1100

    The racing component is basically done. The only thing that needs to be added now now is collisions and obstacles.

---
+++ New Unity Project/Assets/Scripts/Racer2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racer2 : MonoBehaviour
+{
+
+    public float speed = 1f; //Speed can be changed from the inspector.
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal2"), 0, 0);
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 moveAmount = velocity * Time.deltaTime;
+
+        transform.Translate(moveAmount);
+    }
+}
