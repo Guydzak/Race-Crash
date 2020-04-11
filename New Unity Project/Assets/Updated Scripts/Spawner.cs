b7bafea8bbafefd78edd9c6128bb475f5d037ffe
Rohith Maddali <1015083@student.sae.edu.au>
2020-04-11 11:08:25 +1000

    Added spawner for power ups in a new scene. YEt to add pick ups function because We need assets before doing that.

---
+++ New Unity Project/Assets/Updated Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject prefab;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 spawnPos = new Vector2(Random.Range(-4, 5), 2);
+        Instantiate(prefab, spawnPos, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
