198f99915fb13a78f702e37e3ea0102d09d178e6
Rohith Maddali <1015083@student.sae.edu.au>
2020-04-14 11:18:55 +1000

    Added starting timer for the car before moving.

--- New Unity Project/Assets/Updated Scripts/Acceleration.cs
+++ New Unity Project/Assets/Updated Scripts/Acceleration.cs
@@ -10,16 +10,45 @@ public class Acceleration : MonoBehaviour
     float y;
     public float speed = 1;
     public float turnspeed = 1;
+    public int t;
     int temp = 0;
+    public bool firstTime = false;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        rb.constraints = RigidbodyConstraints2D.FreezeAll; // avoids the rigidbody2(Car to fall when idle)
+        StartCoroutine(Move()); //holds the car for the starting point and then starts to accelerate
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (firstTime == true)
+        {
+            rotation = rb.rotation;
+            rb.rotation = rotation - Input.GetAxis("Horizontal2") * turnspeed * Time.deltaTime * 10;
+            rotation = Mathf.Deg2Rad * rotation;
+            x = Mathf.Sin(rotation) * speed * Time.deltaTime * 10;
+            y = Mathf.Cos(rotation) * speed * Time.deltaTime * 10;
+
+            rb.velocity = new Vector2(-x, y);
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                speed = 30;
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                speed = 100;
+            }
+        }
+    }
+
+    IEnumerator Move()
+    {
+        
+        yield return new WaitForSeconds(t);
         rotation = rb.rotation;
         rb.rotation = rotation - Input.GetAxis("Horizontal2") * turnspeed * Time.deltaTime * 10;
         rotation = Mathf.Deg2Rad * rotation;
@@ -28,14 +57,16 @@ public class Acceleration : MonoBehaviour
 
         rb.velocity = new Vector2(-x, y);
 
-        if(Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S))
         {
             speed = 30;
         }
-        else if(Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W))
         {
             speed = 100;
         }
+        firstTime = true; // in order to start the update function after the starting timer.
+        rb.constraints = RigidbodyConstraints2D.None; //Alllows movement for the rigid body2d(Car)
     }
 }
 
