496336f5a1b3d8786f05f5f465c16e60e0ffd025
Rohith Maddali <1015083@student.sae.edu.au>
2020-04-18 22:42:55 +1000

    Different camera script added

---
+++ New Unity Project/Assets/Updated Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform target;
+    Camera myCam;
+    public float speed;
+    float s;
+
+
+    void Start()
+    {
+        myCam = GetComponent<Camera>();
+    }
+
+
+    void Update()
+    {
+
+        
+        s = speed * Time.deltaTime;
+        if(target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, speed) + new Vector3(0,0,-10);
+        }
+    }
+}
